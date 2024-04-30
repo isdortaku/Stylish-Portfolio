30af52e53fbe01cefbb0f1706f2e5ad00028f807
Isdor Taku <isdortakudzwamunengwa@gmail.com>
2024-04-30 04:12:02 -0700

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
 
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+
+const hamburger = document.querySelector('.hamburger');
+const navlinks =  document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
 
 
+hamburger.addEventListener("click", () => {
+navlinks.classList.toggle('open');
+hamburger.classList.toggle('active');
+});
